3e72df4552ee1f0c9befbaea23516ce7c1f5c49b
Lea Pires <lea.pires@heig-vd.ch>
2026-05-04 09:07:46 +0200

    endpoints backend

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/v1/foo', [ApiFooController::class, 'store']);
     Route::get('/v1/polls', [ApiPollController::class, 'index']);
     Route::delete('/v1/polls/{id}', [ApiPollController::class, 'remove']);
+    Route::post('/v1/polls', [ApiPollController::class, 'store']);
+    Route::put('/v1/polls/{id}', [ApiPollController::class, 'update']);
+    Route::post('/v1/polls/{token}/vote', [ApiPollController::class, 'vote']);
 });
+
+Route::get('/v1/polls/{token}/results', [ApiPollController::class, 'results']);
